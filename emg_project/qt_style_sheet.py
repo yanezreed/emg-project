c8823f3637bce0ca0f064b1878fe37475ffd449d
yanezreed <yanezreed@gmail.com>
2026-09-17 18:18:56 +0100

    Qss styling file

---
+++ emg_project/qt_style_sheet.py
@@ -0,0 +1,19 @@
+qss_style_sheet = """
+
+QWidget {font-family: 'Segoe UI'; font-size: 12px;}
+
+QPushButton {padding: 6px; border: 1px solid #CCCCCC; border-radius: 4px; background-color: #F0F0F0; color: #000000;}
+
+QPushButton:hover {background-color: #E0E0E0; border-color: #B0B0B0;}
+
+QLineEdit {padding: 6px; border: 1px solid #CCCCCC; border-radius: 4px;}
+
+QListWidget::item {padding: 1px 0px;}
+
+QMessageBox {min-width: 360px; min-height: 130px;}
+
+QMessageBox QLabel {padding: 8px;}
+
+QMessageBox QPushButton {padding: 3px 12px; min-width: 100px; max-width: 100px; min-height: 28px; max-height: 28px;}
+
+"""
