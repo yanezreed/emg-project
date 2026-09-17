7dfe8f5fcb3b46b83ef8ac5752ade4bb45d51b13
yanezreed <yanezreed@gmail.com>
2026-09-17 17:19:04 +0100

    Sanitised config file

---
+++ emg_project/config.py
@@ -0,0 +1,12 @@
+client_id = "" # <- value removed
+client_secret = "" # <- value removed
+
+api_scope = ["https://api.ebay.com/oauth/api_scope/sell.communication"]
+
+render_callback = "https://emg-project.onrender.com/callback"
+
+render_url = "https://emg-project.onrender.com"
+
+user_ebay_account = "" # <- value removed
+
+ollama_url = "http://localhost:11434/api/generate" # ollama webserver address -> ollama api endpoint
