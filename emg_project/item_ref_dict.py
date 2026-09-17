bd9819d6866012fad6c23571ef355d6b27722ffb
yanezreed <yanezreed@gmail.com>
2026-09-17 18:01:26 +0100

    Temp item dictionary

---
+++ emg_project/item_ref_dict.py
@@ -0,0 +1,7 @@
+item_reference_dict = {
+    "297971683475": "Primaris lieutenant with stalker bolt rifle",
+    "297971679198": "Combat patrol issue zero five",
+    "297971672650": "Primaris lieutenant with storm shield",
+    "297971670421": "Sergeant jovan - Space marine sergeant",}
+    # ref num./manual item name allocation to be overhauled...
+    # ref id will be used to access item name through api call
